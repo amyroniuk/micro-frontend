8718bf0b5eaf7b2b7c404ecdeb58943f5fa00d75
Andrey Myroniuk <andrej.mironjuk@gmail.com>
2020-10-28 12:58:52 +0200

    init frontend

---
+++ src/frontends/frontend/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Frontend = () => {
+    return (
+        <>
+            <View>
+                <Text>Frontend Content</Text>
+            </View>
+        </>
+    );
+};
+
+export default Frontend;
