b458ca3f668190256b88d2108823c9bd49812296
Andrii Myroniuk <amyroniuk@lohika.com>
2020-10-29 12:38:54 +0200

    Navigation

--- src/frontends/frontend/index.js
+++ src/frontends/frontend/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-const Frontend = () => {
+const Frontend = ({navigation}) => {
+    return (
+        <>
+            <View>
+                <Text>Micro Frontend Content</Text>
+                <Button
+                    title="Micro Frontend Child Page"
+                    onPress={() => navigation.navigate('FrontendChild')}
+                />
+            </View>
+        </>
+    );
+};
+
+const FrontendChildScreen = () => {
     return (
         <>
             <View>
@@ -11,4 +25,17 @@ const Frontend = () => {
     );
 };
 
-export default Frontend;
+const Routes = [
+    {
+        name: 'Micro Frontend Home',
+        route: 'Frontend',
+        component: Frontend,
+    },
+    {
+        name: 'Micro Frontend Child',
+        route: 'FrontendChild',
+        component: FrontendChildScreen,
+    },
+];
+
+export default Routes;
